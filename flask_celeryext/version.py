b9341bd7ba9f719d5c1a62074d52886b879676e9
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2015-08-17 17:32:20 +0200

    flask-celeryext v0.1.0
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- flask_celeryext/version.py
+++ flask_celeryext/version.py
@@ -18,4 +18,4 @@ from __future__ import absolute_import, unicode_literals, print_function
 # Do not change the format of this next line. Doing so risks breaking
 # setup.py and docs/conf.py
 
-__version__ = "0.1.0.dev20150817"
+__version__ = "0.1.0"
