02940fe3fa03cca3f03b02e48a61f3e540f602ce
Jiri Kuncar <jiri.kuncar@cern.ch>
2016-02-02 15:52:13 +0100

    version: post-release bump
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- flask_celeryext/version.py
+++ flask_celeryext/version.py
@@ -18,4 +18,4 @@ from __future__ import absolute_import, print_function
 # Do not change the format of this next line. Doing so risks breaking
 # setup.py and docs/conf.py
 
-__version__ = "0.2.0"
+__version__ = "0.2.1.dev20160202"
