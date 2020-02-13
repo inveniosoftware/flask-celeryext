d7f3af9028e0eebdc8f1a48134d928583039b5bc
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-02-13 10:13:45 +0100

    release: v0.3.3

--- flask_celeryext/version.py
+++ flask_celeryext/version.py
@@ -18,4 +18,4 @@ from __future__ import absolute_import, print_function
 # Do not change the format of this next line. Doing so risks breaking
 # setup.py and docs/conf.py
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
